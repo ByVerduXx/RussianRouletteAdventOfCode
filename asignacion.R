1d28b6f248ee202399afaa5754a25ad259f43996
Ediu3095 <eduardoruizsabajanes@gmail.com>
2022-12-02 18:48:37 +0100

    asignacion

--- asignacion.R
+++ asignacion.R
@@ -1,2 +1,15 @@
-lang <- c("JS", "Python", "Java", "C", "C++", "C#", "Rust", "R", "Go", "Swift", "Ruby", "PHP", "Shell", "Scala", "J", "Kotlin", "Objective-C", "Groovy", "Elixir", "Lua", "DM", "Perl", "Dart", "Clojure", "PowerShell", "CoffeeScript", "Haskell", "OCaml", "Lisp", "Vim Script ", "Jsonnet", "Erlang", "Puppet", "Julia", "SmallTalk", "Matlab", "WebAssembly", "Fortran", "F#", "VisualBasics", "Apex", "ABAP", "Vala ", "Nix", "Crystal", "Roff", "Perl 6", "FreeMaker", "PureScript", "Objective C++")
-paste(c("Edu", "Jesus", "Alex", "Dani"), "-", sample(lang, 4))
+# Lenguajes y participantes
+# NO QUITAR NINGUNO
+languages <- c("JS", "Python", "Java", "C", "C++", "C#", "Rust", "R", "Go", "Swift", "Ruby", "PHP", "Shell", "Scala", "J", "Kotlin", "Objective-C", "Groovy", "Elixir", "Lua", "DM", "Perl", "Dart", "Clojure", "PowerShell", "CoffeeScript", "Haskell", "OCaml", "Lisp", "Vim Script", "Jsonnet", "Erlang", "Puppet", "Julia", "SmallTalk", "Matlab", "WebAssembly", "Fortran", "F#", "VisualBasics", "Apex", "ABAP", "Vala", "Nix", "Crystal", "Roff", "Perl 6", "FreeMaker", "PureScript", "Objective C++")
+participants <- c("Edu", "Jesus", "Alex", "Dani", "Checo", "Divy", "Oscar", "Hugo")
+
+# Eleccion de lenguajes para todos los participantes, todos los dias
+set.seed(42)
+df <- as.data.frame(sapply(participants, function(x) sample(languages, 25, TRUE)))
+
+# Elecciones del dia 1 y 2 (las calculamos de otra forma así que las forzamos)
+df[1:2, 1:5] <- matrix(c("Fortran", "F#", "PureScript", "Groovy", "PowerShell", "PureScript", "Kotlin", "Perl", "Apex", "Vim Script"), 2, 5)
+
+# Elecciones para el dia n
+n <- 1:2
+df[n, ]
